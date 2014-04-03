c4cc1db87b37c968224ac2a5dd3afd7b931c4453
Jon Bodner <jon@bodnerfamily.com>
2014-04-03 16:37:04 -0400

    Update ctrl.js

--- app/js/ctrl.js
+++ app/js/ctrl.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * Copyright 2000-2014 NeuStar, Inc. All rights reserved.
+ * NeuStar, the Neustar logo and related names and logos are registered
+ * trademarks, service marks or tradenames of NeuStar, Inc. All other
+ * product names, company names, marks, logos and symbols may be trademarks
+ * of their respective owners.
+ */
 
 
 function Ctrl($scope, $http, $parse, $resource) {
