ec323f6d7047477bcd26b7a02a6b91e9e07736e8
Jon Bodner <jon@bodnerfamily.com>
2014-04-03 16:37:53 -0400

    Update status.js

--- app/js/status.js
+++ app/js/status.js
@@ -1,4 +1,13 @@
 'use strict';
+
+/**
+ * Copyright 2000-2014 NeuStar, Inc. All rights reserved.
+ * NeuStar, the Neustar logo and related names and logos are registered
+ * trademarks, service marks or tradenames of NeuStar, Inc. All other
+ * product names, company names, marks, logos and symbols may be trademarks
+ * of their respective owners.
+ */
+
 function StatusCtrl($scope, $http, $parse, $resource) {
     function prepareStatusResource() {
         return $scope.prepareResource('/status');
