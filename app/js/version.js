6161f6166f74b0cb34710670e2955323031bd3f7
Jon Bodner <jon@bodnerfamily.com>
2014-04-03 16:38:02 -0400

    Update version.js

--- app/js/version.js
+++ app/js/version.js
@@ -1,4 +1,13 @@
 'use strict';
+
+/**
+ * Copyright 2000-2014 NeuStar, Inc. All rights reserved.
+ * NeuStar, the Neustar logo and related names and logos are registered
+ * trademarks, service marks or tradenames of NeuStar, Inc. All other
+ * product names, company names, marks, logos and symbols may be trademarks
+ * of their respective owners.
+ */
+
 function VersionCtrl($scope, $http, $parse, $resource) {
     function prepareVersionResource() {
         return $scope.prepareResource('/version');
